9245d4c98f76b491b2b376c39502f1ebe417de49
chhunly <60867999+chhunlyyy@users.noreply.github.com>
2023-03-23 23:04:56 +0700

    add script

---
+++ gr-md.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+read -p "Enter Module Name : " v
+
+mkdir -p $v
+
+mkdir -p $v/services
+echo > $v/services/${v}_service.dart
+
+mkdir -p $v/screens
+echo > $v/screens/${v}_screen.dart
+
+mkdir -p $v/stores
+echo > $v/stores/${v}_store.dart
+
+mkdir -p $v/models
+echo > $v/models/${v}_model.dart
