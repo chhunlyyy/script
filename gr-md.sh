46ba5f2394fcf3325acfb30309c7407c49b60526
Serey Chhunly <chhunly.serey@poscardigital.com>
2023-03-25 15:12:44 +0700

    test

--- gr-md.sh
+++ gr-md.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 clear
-mkdir -p $1
+mkdir -p abc
 
-mkdir -p $1/services
-echo > $1/services/${1}_service.dart
+mkdir -p abc/services
+echo > abc/services/abc_service.dart
 
-mkdir -p $1/screens
-echo > $1/screens/${1}_screen.dart
+mkdir -p abc/screens
+echo > abc/screens/abc_screen.dart
 
-mkdir -p $1/stores
-echo > $1/stores/${1}_store.dart
+mkdir -p abc/stores
+echo > abc/stores/abc_store.dart
 
-mkdir -p $1/models
-echo > $1/models/${1}_model.dart
+mkdir -p abc/models
+echo > abc/models/abc_model.dart
