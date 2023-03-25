d08c0192e185f887fa5fcb4f2ffa4125a86d8698
Serey Chhunly <chhunly.serey@poscardigital.com>
2023-03-25 14:37:14 +0700

    test

--- gr-md.sh
+++ gr-md.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
 
-# clear
-# read -p "Enter Module Name : " v
-
-# mkdir -p $v
-
-# mkdir -p $v/services
-# echo > $v/services/${v}_service.dart
-
-# mkdir -p $v/screens
-# echo > $v/screens/${v}_screen.dart
-
-# mkdir -p $v/stores
-# echo > $v/stores/${v}_store.dart
-
-# mkdir -p $v/models
-# echo > $v/models/${v}_model.dart
-
-
 clear
-# read -p "Enter Module Name : " v
+read -p "Enter Module Name : " v
 
-mkdir -p abc
+mkdir -p $v
 
-mkdir -p abc/services
-echo > abc/services/abc_service.dart
+mkdir -p $v/services
+echo > $v/services/${v}_service.dart
 
-mkdir -p abc/screens
-echo > abc/screens/abc_screen.dart
+mkdir -p $v/screens
+echo > $v/screens/${v}_screen.dart
+
+mkdir -p $v/stores
+echo > $v/stores/${v}_store.dart
+
+mkdir -p $v/models
+echo > $v/models/${v}_model.dart
